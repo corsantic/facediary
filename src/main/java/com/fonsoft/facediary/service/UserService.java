6dd6fa55ec242eb654e4a95acfb424cfda9e9d49
oguzhangur <oguz@arintel.net>
2020-09-06 21:53:33 +0300

    add service interface and implementation

--- src/main/java/com/fonsoft/facediary/service/UserService.java
+++ src/main/java/com/fonsoft/facediary/service/UserService.java
@@ -1,4 +1,13 @@
 package com.fonsoft.facediary.service;
 
+import com.fonsoft.facediary.model.User;
+import com.fonsoft.facediary.model.UserParameter;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface UserService {
+
+    User authenticate(UserParameter userParameter);
 }
+
+
--- src/main/java/com/fonsoft/facediary/service/impl/UserServiceImpl.java
+++ src/main/java/com/fonsoft/facediary/service/impl/UserServiceImpl.java
@@ -1,8 +1,27 @@
 package com.fonsoft.facediary.service.impl;
 
+import com.fonsoft.facediary.model.User;
+import com.fonsoft.facediary.model.UserParameter;
+import com.fonsoft.facediary.repository.UserRepository;
 import com.fonsoft.facediary.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
+//
+//
+//
+//    public boolean validateUserInfo(String userName, String userPassword){
+//        User user = userRepository.findById()
 
+    private UserRepository userRepository;
 
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User authenticate(UserParameter userParameter) {
+        return userRepository.authenticate(userParameter);
+    }
 }
