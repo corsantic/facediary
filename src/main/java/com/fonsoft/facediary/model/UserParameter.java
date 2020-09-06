58a353a3fc72fac9780dc7ab2fb8848cd39e552a
oguzhangur <oguz@arintel.net>
2020-09-06 21:52:48 +0300

    add class for user parameters

---
+++ src/main/java/com/fonsoft/facediary/model/UserParameter.java
@@ -0,0 +1,29 @@
+package com.fonsoft.facediary.model;
+
+//TODO: add lombok functionality
+
+public class UserParameter {
+    private String userName;
+    private String userPassword;
+
+    public UserParameter(String userName, String userPassword) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
