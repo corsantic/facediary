94406722c05ddbb353e29528cd8e344c0ccf9964
oguzhangur <oguz@arintel.net>
2020-09-06 21:52:02 +0300

    update User model with annotations

--- src/main/java/com/fonsoft/facediary/model/User.java
+++ src/main/java/com/fonsoft/facediary/model/User.java
@@ -3,14 +3,38 @@ package com.fonsoft.facediary.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 
 @Getter
 @Setter
+
+@Entity
 public class User {
-    private int id;
-    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userName;
+    private String userPassword;
+    private String userFacePassword;
 
     public User() {
     }
 
+    public User(String userName, String userPassword, String userFacePassword) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userFacePassword = userFacePassword;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", userFacePassword='" + userFacePassword + '\'' +
+                '}';
+    }
 }
