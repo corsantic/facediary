d3f03014ae08f1bd4f69b17cf1dc99173489099e
oguzhangur <oguz@arintel.net>
2020-09-08 18:43:19 +0300

    update hibernate config to use mysql

--- src/main/java/com/fonsoft/facediary/configuration/HibernateConfiguration.java
+++ src/main/java/com/fonsoft/facediary/configuration/HibernateConfiguration.java
@@ -36,7 +36,7 @@ public class HibernateConfiguration {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getRequiredProperty("spring.datasource.driverClassName"));
+//        dataSource.setDriverClassName(environment.getRequiredProperty("spring.datasource.driverClassName"));
         dataSource.setUsername(environment.getRequiredProperty("spring.datasource.username"));
         dataSource.setUrl(environment.getRequiredProperty("spring.datasource.url"));
         dataSource.setPassword(environment.getRequiredProperty("spring.datasource.password"));
@@ -45,7 +45,7 @@ public class HibernateConfiguration {
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        addPropertiesWithMessage(properties, "spring.jpa.database-platform");
+//        addPropertiesWithMessage(properties, "spring.jpa.database-platform");
 //        addPropertiesWithMessage(properties, "hibernate.format_sql");
 //        addPropertiesWithMessage(properties, "hibernate.show_sql");
 //        addPropertiesWithMessage(properties, "hibernate.hbm2ddl.auto");
